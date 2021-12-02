eecda3fe390c0407825fb15bd0d00619ffaf9f33
alikasundara <5837695+alikasundara@users.noreply.github.com>
2021-12-02 17:25:05 +0000

    don't check certs, they won't be known anyway

--- bt-wifi.sh
+++ bt-wifi.sh
@@ -18,7 +18,7 @@ BASE_URL="https://www.btopenzone.com:8443"
 LOGOUT_URL="$BASE_URL/accountLogoff/home?confirmed=true"
 
 if [ "$1" == "--logout" ]; then
-	curl -A "$USER_AGENT" "$LOGOUT_URL" || echo "${RED_BEGIN}Failed to log you out${NORMAL}"
+	curl --insecure -A "$USER_AGENT" "$LOGOUT_URL" || echo "${RED_BEGIN}Failed to log you out${NORMAL}"
 else
 
 	USERNAME=$1
@@ -26,11 +26,11 @@ else
 	CHECK_STATUS_URL="$BASE_URL/home"
 	LOGIN_URL="$BASE_URL/ante"
 
-	if curl -A "$USER_AGENT" "$CHECK_STATUS_URL" 2>/dev/null | grep -q "re now logged on to BT Wi-Fi"; then
+	if curl --insecure -A "$USER_AGENT" "$CHECK_STATUS_URL" 2>/dev/null | grep -q "re now logged on to BT Wi-Fi"; then
 		echo "${GREEN_BEGIN}you're logged in${NORMAL}"
 	else
 		echo "Trying to log you in..."
-		if wget -qO - --header='Accept: text/html' --user-agent "$USER_AGENT" --post-data "inputUsername=${USERNAME}&username=wam/${USERNAME}&password=${PASSWORD}" $LOGIN_URL 2>/dev/null | grep -q "re now logged on to BT Wi-Fi"; then
+		if wget --no-check-certificate -qO - --header='Accept: text/html' --user-agent "$USER_AGENT" --post-data "inputUsername=${USERNAME}&username=wam/${USERNAME}&password=${PASSWORD}" $LOGIN_URL 2>/dev/null | grep -q "re now logged on to BT Wi-Fi"; then
 			echo "${GREEN_BEGIN}you've been logged in${NORMAL}"
 		else
 			echo "${RED_BEGIN}Something went wrong${NORMAL}"
